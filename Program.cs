dd1757014a3591991089893338c3525f9f63b246
maksie <dimedmax95@gmail.com>
2024-04-09 20:27:50 +0400

    Подсчет строк

--- Program.cs
+++ Program.cs
@@ -2,9 +2,22 @@
 
         
         string[] resultArray = FilterStrings(inputArray);
-        
+
         Console.WriteLine("Результат:");
         foreach (string str in resultArray)
         {
             Console.WriteLine(str);
         }
+
+static string[] FilterStrings(string[] inputArray)
+    {
+        int count = 0; 
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+       
